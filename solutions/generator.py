288d3201b78d052b09a42a82be68320420eff28a
Yi <vi@x.com>
2025-05-28 14:47:16 +0300

    add sulution to generator.

--- solutions/generator.py
+++ solutions/generator.py
@@ -1,15 +1,17 @@
-# def generate_numbers():
-#     # TODO implement
-#     pass
+def generate_numbers():
+    n = 0
+    while True:
+        n += 1
+        yield n
 
 
-class generate_numbers:
-    def __init__(self):
-        self.c = 0
+# class generate_numbers:
+#     def __init__(self):
+#         self.c = 0
 
-    def __next__(self):
-        self.c += 1
-        return self.c
+#     def __next__(self):
+#         self.c += 1
+#         return self.c
 
 
 if __name__ == "__main__":
